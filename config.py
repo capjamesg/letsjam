602c2ffa713a768686318b413f5bd7899f96e732
capjamesg <jamesg@jamesg.blog>
2022-01-07 07:15:42 +0000

    add example config.py and a requirements.txt file https://github.com/capjamesg/letsjam/pull/2

---
+++ config.py
@@ -0,0 +1,29 @@
+site_config = {
+    "title": "Blog Title",
+    "posts": [],
+    "categories": {},
+    "months": [],
+    "years": [],
+    "likes": [],
+    "bookmarks": [],
+    "replies": [],
+    "reposts": [],
+    "drinking": [],
+    "rsvps": [],
+    "baseurl": "https://example.com",
+    "pages": [],
+    "webmentions": [],
+    "wiki": []
+}
+
+BASE_DIR = "."
+OUTPUT = "_site"
+ALLOWED_SLUG_CHARS = ["-", "/", ".", "_"]
+
+# enable auto-generated page types
+# these values must be either True or False, or an error will be thrown
+
+CATEGORY_PAGE_GENERATION = True
+PAGINATION_PAGE_GENERATION = True
+DATE_ARCHIVE_GENERATION = True
+LIST_PAGE_GENERATION = True
